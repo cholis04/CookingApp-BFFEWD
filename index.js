fa93287fcf0d23429a6b04abb8c3e1c746454974
Nurcholis <64176670+cholis04@users.noreply.github.com>
2021-08-13 16:11:00 +0700

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-import './components/Header.js'
-import './components/SearchBox.js'
-import './components/MainContent.js'
-import './components/Footer.js'
-
-import app from './app.js'
-
-document.addEventListener('DOMContentLoaded', app)
